ff4bd2c21317c0008a7c3b1d4d72412810f6b5fe
Oksana Yakupova <mail.oksanayakupova@gmail.com>
2019-11-25 15:13:01 +0200

    feat: add .babelrc, jest.config.js, add webpack configuration

---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/scripts/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // include: [path.resolve(__dirname, 'src/styles/style.css')],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: false,
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css',
+        }),
+    ],
+};
